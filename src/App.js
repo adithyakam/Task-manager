854719313d0413fb2bee7bae61782b3e4a2ebcd2
adithyakam <50355901+adithyakam@users.noreply.github.com>
2020-10-07 23:01:44 +0530

    fixing boards

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ import "./App.css";
 function App() {
   const [boards, setBoards] = useState({ a: {}, b: {} });
 
+  const addBoard = ({ name, event }) => {
+    event.preventDefault();
+
+    console.log(name, "asf");
+
+    setBoards(name);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Boards boards={boards} />
+      <Boards boards={boards} addBoard={addBoard} />
     </div>
   );
 }
--- src/Components/Boards/Boards.component.jsx
+++ src/Components/Boards/Boards.component.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Boards.css";
 
 function Boards(props) {
-  const { boards } = props;
+  const [name, setName] = useState("");
+
+  const setBoardName = (e) => {
+    setName(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const { boards, addBoard } = props;
   return (
-    <div className="boards">
-      {Object.keys(boards).map((ele) => {
-        return (
-          <div className="ip_board">
-            <h1>{ele}</h1>
-          </div>
-        );
-      })}
+    <div className="main_boards">
+      <form onSubmit={(event) => addBoard(name, event)}>
+        <label>new board name</label>
+        <input onChange={setBoardName} />
+      </form>
+      <div className="boards">
+        {Object.keys(boards).map((ele) => {
+          return (
+            <div className="ip_board">
+              <h1>{ele}</h1>
+            </div>
+          );
+        })}
+        <div className="ip_board">
+          <h1>{name}</h1>
+        </div>
+      </div>
     </div>
   );
 }
